0d49b9601bde6aa605887861867df46cf0125a51
Xharles <charlesnzubechukwu000@gmail.com>
2022-03-17 22:30:54 +0100

    rename ontainer to wrapper

--- src/container/MapWrapper.jsx
+++ src/container/MapWrapper.jsx
@@ -14,48 +14,15 @@ import nigeriaStatesGeoJSON from "../data/nigeria-states.json";
 
 const zoom = 6;
 const location = [9, 8];
-const FUTA = [7.307, 5.1398];
 
-function LocationMarker() {
-  const [position, setPosition] = useState(null);
-  const map = useMapEvents({
-    click() {
-      map.locate();
-    },
-    locationfound(e) {
-      setPosition(e.latlng);
-      map.flyTo(e.latlng, map.getZoom());
-      console.log(e);
-    },
-  });
-
-  return position === null ? null : (
-    <Marker position={position}>
-      <Popup>You are here</Popup>
-    </Marker>
-  );
-}
 
 export default function MapWrapper() {
   // https://github.com/PaulLeCam/react-leaflet/issues/841
   // useRef doesn't work so we have to save the map instance.
   const [mapRefObject, setMapRefObject] = useState();
 
-  useEffect(() => {
-    if (mapRefObject)
-      setTimeout(() => {
-        mapRefObject?.flyTo(FUTA, 14, {
-          duration: 2.5,
-        });
-      }, 1500);
-    nigeriaStatesGeoJSON;
-  }, [mapRefObject]);
-
   return (
     <MapContainer
-      whenCreated={(mapInstance) => {
-        setMapRefObject(mapInstance);
-      }}
       className="markercluster-map"
       center={location}
       zoom={zoom}
@@ -66,10 +33,8 @@ export default function MapWrapper() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      {/* <Marker position={location} /> */}
       <ZoomControl position="bottomright" />
       <GeoJSON data={nigeriaStatesGeoJSON} />
-      {/* <LocationMarker /> */}
     </MapContainer>
   );
 }
