8381eeb8a5636c0e034c4d89d10952fd9f3085b9
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-03-29 11:32:05 +0100

    render bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/css/zobomap.css";
-import Router from "./router.jsx";
+import Router from "./Router.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
