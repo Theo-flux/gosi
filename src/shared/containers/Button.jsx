339bd56db11a34dce41b5d784ccc88309a5505b7
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-03-30 15:31:17 +0100

    publications component

--- src/shared/containers/Button.jsx
+++ src/shared/containers/Button.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
-const Button = ({children, onClick}) => {
+const Button = ({children, className, ...restProps}) => {
     return(
-        <button onClick={onClick} className={"outline-0 text-sm text-white rounded-lg py-2 px-4 md:py-4 md:px-8 font-medium font-gilmer bg-primary-600 transition-all duration-300 hover:bg-white hover:text-primary-600 hover:ring-2 hover:ring-primary-600"}>
+        <button {...restProps} className={`outline-0 text-sm text-white rounded-lg py-2 px-4 md:py-4 md:px-8 font-medium font-gilmer bg-primary-600 transition-all duration-300 hover:bg-white hover:text-primary-600 hover:ring-2 hover:ring-primary-600 ${className}`}>
             {children}
         </button>
     );
 }
 
-export { Button };
+const ButtonWithIcon = ({children, className, ...restProps}) => {
+    return(
+        <button {...restProps} className={`outline-0 text-sm text-primary-600 rounded-lg py-2 px-4 md:py-4 md:px-8 font-medium font-gilmer bg-primary-100 transition-all duration-300 hover:bg-white hover:ring-2 hover:ring-primary-600 ${className}`}>
+            {children}
+        </button>
+    );
+}
+
+export { 
+    Button,
+    ButtonWithIcon
+ };
