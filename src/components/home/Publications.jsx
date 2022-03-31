11d860941cb347b1c359abd4fa8e6b309ef6d00f
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-03-31 14:42:18 +0100

    publications.jsx refactoring

--- src/components/home/Publications.jsx
+++ src/components/home/Publications.jsx
@@ -1,5 +1,32 @@
-import { publications } from "./publicationsData";
 import { Section, Div, Title, SubTitle, SmallText, ButtonWithIcon } from "../../shared";
+
+
+import pubImage1 from "../../images/home/publication_1.png";
+import pubImage2 from "../../images/home/publication_2.png";
+import pubImage3 from "../../images/home/publication_3.png";
+
+const publications = [
+    {
+        id: "1",
+        image: pubImage1,
+        source: "cjid.com",
+        data: "Type in the name or address of a place. Type in the name or address of a place. Type in the name or address of a place."
+    },
+
+    {
+        id: "2",
+        image: pubImage2,
+        source: "cjid.com",
+        data: "Type in the name or address of a place. Type in the name or address of a place. Type in the name or address of a place."
+    },
+
+    {
+        id: "3",
+        image: pubImage3,
+        source: "cjid.com",
+        data: "Type in the name or address of a place. Type in the name or address of a place. Type in the name or address of a place."
+    }
+];
 
 const PublicationCard = ({pubData}) => {
     const {id, image, source, data} = pubData
