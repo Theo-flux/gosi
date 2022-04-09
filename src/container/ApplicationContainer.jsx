1ed195ed569065ff99d6e20c2576e23272e3e37b
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-04-09 17:53:18 +0100

    functionality fix

--- src/container/ApplicationContainer.jsx
+++ src/container/ApplicationContainer.jsx
@@ -15,13 +15,13 @@ function ApplicationContainer({locationLevel, countryResult, stateResult, lgaRes
           <ExploreNav handleShowchart={handleShowchart} className="absolute w-full z-[403] bg-white"/>
           <MenuFold onClick={() => handleShowchart()} className={`sm:hidden md:flex md:justify-center md:items-center md:absolute md:z-[405] md:bg-white drop-shadow-md md:w-[40px] md:h-[40px] ${showChart ? "md:left-[864px] md:top-[45px]" :"md:left-[245px] md:top-[45px]"} md:transition-all md:duration-300`}/>
           {
-            showChart && <GraphicalData className={`absolute border-t border-t-[#EAEAEA] h-auto z-[403] bg-white top-[67px] md:z-[402] 
+            showChart && <GraphicalData className={`absolute border-t border-t-[#EAEAEA] h-auto z-[404] bg-white top-[67px] md:z-[402] 
             ${showChart ? "left-0 md:left-[260px]" :"md:left-[-300px]"} transition-all duration-300`}/>      
           }
 
           {
             showChart && 
-            <div onClick={() => handleShowchart()} className={`${showChart ? "left-0" : "left-[-400px]"} transition-all duration-200 absolute top-0 w-full h-[100vh] z-[405] md:z-[402] md:hidden`}>
+            <div onClick={() => handleShowchart()} className={`${showChart ? "left-0" : "left-[-400px]"} transition-all duration-200 absolute top-0 w-full h-[100vh] z-[403] md:hidden`}>
 
             </div>
           }
