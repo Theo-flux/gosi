543578ede1dcefc76a236d3c5c32e1e289fcb326
Xharles <charlesnzubechukwu000@gmail.com>
2022-04-14 12:06:10 +0100

    zoom level

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,8 @@
 export const stateSuffix = "-state";
 export const lgaSuffix = "-lga";
 export const locationLevels = {
-  one: { name: "country", zoom: 7 },
-  two: { name: "state", zoom: 8 },
-  three: { name: "lga", zoom: 9 },
+  home: { name: "country", mobileZoom: 6, zoom: 6 },
+  one: { name: "country", mobileZoom: 6, zoom: 7 },
+  two: { name: "state", mobileZoom: 7, zoom: 8 },
+  three: { name: "lga", mobileZoom: 8, zoom: 9 },
 };
